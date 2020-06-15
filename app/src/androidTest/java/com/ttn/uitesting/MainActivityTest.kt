98e879e45f41924e75e2902b302cc5fa6f3b67bf
naveen-verma-TTN <naveen.verma@tothenew.com>
2020-06-15 14:37:13 +0530

    UI testing - Testing navigation from one activity to another activity

--- app/src/androidTest/java/com/ttn/uitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/ttn/uitesting/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.ttn.uitesting
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -10,6 +12,39 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
+
+    /**
+     * Navigation test: MainActivity -> SecondaryActivity
+     */
+    @Test
+    fun test_navSecondaryActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity))
+                .perform(click())
+
+        onView(withId(R.id.secondary))
+                .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPress_toMainActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity))
+                .perform(click())
+
+        onView(withId(R.id.secondary))
+                .check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back))
+                .perform(click()) // method 1
+
+//        pressBack() // method 2
+
+        onView(withId(R.id.main))
+                .check(matches(isDisplayed()))
+    }
 
     /**
      * fun to check if the activity is render or not
